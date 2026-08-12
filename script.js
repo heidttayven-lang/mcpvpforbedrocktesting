218ef141e641f452bc54e3b81cad75f2a88dd655
heidttayven-lang <heidt.tayven@prairiesouth.ca>
2026-08-12 02:51:04 -0700

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-world.afterEvents.itemUse.subscribe(event => {
-    const { itemStack, source } = event;
-    if (itemStack?.typeId === MinecraftItemTypes.diamondSword.id && itemStack.nameTag === "Duels") {
-        source.sendMessage("You are holding the Duels sword!");
-    }
-});
